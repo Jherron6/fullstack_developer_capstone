a92a358f884a0a7c0be49c9fdcbdac22cdcb5bb9
Jherron6 <142568776+Jherron6@users.noreply.github.com>
2025-09-07 15:58:36 -0500

    Update views.py
    removed ", post_review" from line 10

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments
 
 
 # Get an instance of a logger
@@ -36,15 +36,15 @@ def login_user(request):
 # Create a `logout_request` view to handle sign out request
 @csrf_exempt
 def logout_request(request):
-    logout(request) # Terminate user session
-    data = {"userName":""} # Return empty username
+      # logout(request) Terminate user session
+    data = {"userName": ""}  # Return empty username
     return JsonResponse(data)
 
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+      # context = {}
 
     # Load JSON data from the request body
     data = json.loads(request.body)
@@ -54,7 +54,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    #email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -69,18 +69,19 @@ def registration(request):
         user = User.objects.create_user(
             username=username,
             first_name=first_name,
-            last_name=last_name,password=password,
+            last_name=last_name,
+            password=password,
             email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {"userName" : username, "status" : "Authenticated"}
         return JsonResponse(data)
     else :
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
-#get a list of cars
+# get a list of cars
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
@@ -89,7 +90,12 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name
+            }
+        )
     return JsonResponse({"CarModels": cars})
 
 """
