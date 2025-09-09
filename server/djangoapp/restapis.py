049a691faa0eb9a64d9af251d1fee726fe8e66d8
Jennifer Herron <sapharai@gmail.com>
2025-09-09 00:05:04 -0400

    Testing successful

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -32,7 +32,7 @@ def get_request(endpoint, **kwargs):
 
 # for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url+"analyze/"+str(text).replace(" ", "%20")
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
